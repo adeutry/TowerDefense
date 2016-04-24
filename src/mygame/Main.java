875c7b04f4849d5faaae990d52df156da541713f
adeutry <adeutry@temple.edu>
2016-04-24 19:00:48 -0400

    added a screen for selecting difficulty

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -32,162 +32,162 @@ import java.util.ArrayList;
  */
 public class Main extends SimpleApplication {
 
-    private static Dimension screen;
-    BitmapText stateInfoText;
-    private NiftyJmeDisplay niftyDisplay;
-    private Nifty nifty;
-		public ArrayList<Tower> towers;
-		public ArrayList<Enemy> enemies;
-		public int enemyCount = 0;
-                 /**
-     * Physics for collision
+  private static Dimension screen;
+  BitmapText stateInfoText;
+  private NiftyJmeDisplay niftyDisplay;
+  private Nifty nifty;
+  public ArrayList<Tower> towers;
+  public ArrayList<Enemy> enemies;
+  public int enemyCount = 0;
+  public String difficulty = "";
+  /**
+   * Physics for collision
+   */
+  public BulletAppState bulletAppState;
+  public Tower homeTower;
+  Material whiteGlow;
+  Material laserGlow;
+
+  public static void main(String[] args) {
+    Main app = new Main();
+    initAppScreen(app);
+    app.start();
+  }
+
+  @Override
+  public void simpleInitApp() {
+
+    initMaterials();
+    initLights();
+    initModels();
+    initGui();
+
+    //MainTower
+    initCrossHairs();
+    /**
+     * Set up Physics Game
      */
-    public BulletAppState bulletAppState;
-		public Tower homeTower;
-		
-		Material whiteGlow;
-		Material laserGlow;
+    bulletAppState = new BulletAppState();
+    stateManager.attach(bulletAppState);
 
-    public static void main(String[] args) {
-        Main app = new Main();
-        initAppScreen(app);
-        app.start();
-    }
 
-    @Override
-    public void simpleInitApp() {
+    StartScreenState startScreen = new StartScreenState();
+    stateManager.attach(startScreen);
 
-        initMaterials();
-        initLights();
-        initModels();
-        initGui();
-        
-        //MainTower
-        initCrossHairs();
-         /**
-         * Set up Physics Game
-         */
-        bulletAppState = new BulletAppState();
-        stateManager.attach(bulletAppState);
-        
-
-        StartScreenState startScreen = new StartScreenState();
-				stateManager.attach(startScreen);
-			
 //				TestState testState = new TestState();
 //        stateManager.attach(testState);
-    }
+  }
 
-    private void initMaterials() {
-				
-				//glow for enemies
-				whiteGlow = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-				whiteGlow.setColor("Color", ColorRGBA.Cyan);
-				whiteGlow.setColor("GlowColor", ColorRGBA.White);
-				FilterPostProcessor fpp=new FilterPostProcessor(assetManager);
-				BloomFilter bloom= new BloomFilter(BloomFilter.GlowMode.Objects);        
-				bloom.setBloomIntensity(3.0f);
-				bloom.setBlurScale(7.0f);
-				fpp.addFilter(bloom);
-				viewPort.addProcessor(fpp);
-				
-				//glow for laser beams
-				laserGlow = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-				laserGlow.setColor("Color", ColorRGBA.Red);
-				laserGlow.setColor("GlowColor", ColorRGBA.Red);
-			  
-				
-    }
+  private void initMaterials() {
 
-    private void initLights() {
-    }
+    //glow for enemies
+    whiteGlow = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+    whiteGlow.setColor("Color", ColorRGBA.Cyan);
+    whiteGlow.setColor("GlowColor", ColorRGBA.White);
+    FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+    BloomFilter bloom = new BloomFilter(BloomFilter.GlowMode.Objects);
+    bloom.setBloomIntensity(3.0f);
+    bloom.setBlurScale(7.0f);
+    fpp.addFilter(bloom);
+    viewPort.addProcessor(fpp);
 
-    private void initModels() {
-				
-				//initialize towers and enemies arrays
-				towers = new ArrayList<Tower>();
-				enemies = new ArrayList<Enemy>();
-				
-        //attach skybox
-        Spatial sky = SkyFactory.createSky(assetManager, "Textures/Sky/BlueClouds.dds", false);
-        rootNode.attachChild(sky);
+    //glow for laser beams
+    laserGlow = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+    laserGlow.setColor("Color", ColorRGBA.Red);
+    laserGlow.setColor("GlowColor", ColorRGBA.Red);
 
-    }
 
-    private void initGui() {
-        //create the state info break in the top right
-        BitmapFont bmf = this.getAssetManager().loadFont("Interface/Fonts/ArialBlack.fnt");
-        stateInfoText = new BitmapText(bmf);
-        stateInfoText.setSize(bmf.getCharSet().getRenderedSize() * 1f);
-        stateInfoText.setColor(ColorRGBA.White);
-        stateInfoText.setText("");
-        stateInfoText.setLocalTranslation(10f, this.getAppSettings().getHeight() - 50, 0f);
-        this.getGuiNode().attachChild(stateInfoText);
+  }
 
-        niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, guiViewPort);
-        nifty = niftyDisplay.getNifty();
-    }
+  private void initLights() {
+  }
 
-    public Dimension getScreenDimension() {
-        return this.screen;
-    }
+  private void initModels() {
 
-    public AppSettings getAppSettings() {
-        return this.settings;
-    }
+    //initialize towers and enemies arrays
+    towers = new ArrayList<Tower>();
+    enemies = new ArrayList<Enemy>();
 
-    public Nifty getNifty() {
-        return this.nifty;
-    }
+    //attach skybox
+    Spatial sky = SkyFactory.createSky(assetManager, "Textures/Sky/BlueClouds.dds", false);
+    rootNode.attachChild(sky);
 
-    public NiftyJmeDisplay getNiftyDisplay() {
-        return this.niftyDisplay;
-    }
+  }
 
-    public InputManager getInputManager() {
-        return this.inputManager;
-    }
+  private void initGui() {
+    //create the state info break in the top right
+    BitmapFont bmf = this.getAssetManager().loadFont("Interface/Fonts/ArialBlack.fnt");
+    stateInfoText = new BitmapText(bmf);
+    stateInfoText.setSize(bmf.getCharSet().getRenderedSize() * 1f);
+    stateInfoText.setColor(ColorRGBA.White);
+    stateInfoText.setText("");
+    stateInfoText.setLocalTranslation(10f, this.getAppSettings().getHeight() - 50, 0f);
+    this.getGuiNode().attachChild(stateInfoText);
 
-    private static void initAppScreen(SimpleApplication app) {
-        //set screen settings 
-        AppSettings aps = new AppSettings(true);
-        screen = Toolkit.getDefaultToolkit().getScreenSize();
-        screen.width *= 0.80;
-        screen.height *= 0.75;
-        aps.setResolution(screen.width, screen.height);
-        app.setSettings(aps);
-				
-        //get rid of initial jmonkey screen
-        app.setShowSettings(false);
-				
-				
-    }
+    niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, guiViewPort);
+    nifty = niftyDisplay.getNifty();
+  }
 
-    public void deleteInputMappings(String mappings[]) {
-        for (String i : mappings) {
-            inputManager.deleteMapping(i);
-        }
-    }
+  public Dimension getScreenDimension() {
+    return this.screen;
+  }
 
-    public boolean hasObject(LinkedList<?> sp, Class cls) {
-        for (Object s : sp) {
-            if (s.getClass().equals(cls)) {
-                return true;
-            }
-        }
-        return false;
+  public AppSettings getAppSettings() {
+    return this.settings;
+  }
+
+  public Nifty getNifty() {
+    return this.nifty;
+  }
+
+  public NiftyJmeDisplay getNiftyDisplay() {
+    return this.niftyDisplay;
+  }
+
+  public InputManager getInputManager() {
+    return this.inputManager;
+  }
+
+  private static void initAppScreen(SimpleApplication app) {
+    //set screen settings 
+    AppSettings aps = new AppSettings(true);
+    screen = Toolkit.getDefaultToolkit().getScreenSize();
+    screen.width *= 0.80;
+    screen.height *= 0.75;
+    aps.setResolution(screen.width, screen.height);
+    app.setSettings(aps);
+
+    //get rid of initial jmonkey screen
+    app.setShowSettings(false);
+
+
+  }
+
+  public void deleteInputMappings(String mappings[]) {
+    for (String i : mappings) {
+      inputManager.deleteMapping(i);
     }
-    
-      //for mainTower crosshair
-     public void initCrossHairs() {
-        guiNode.detachAllChildren();
-        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
-        BitmapText ch = new BitmapText(guiFont, false);
-        ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
-        ch.setText("+");       
-        ch.setLocalTranslation( // center
-                settings.getWidth() / 2 - guiFont.getCharSet().getRenderedSize() / 3 * 2,
-                settings.getHeight() / 2 + ch.getLineHeight() / 2 + -25, 0);
-        guiNode.attachChild(ch);
+  }
+
+  public boolean hasObject(LinkedList<?> sp, Class cls) {
+    for (Object s : sp) {
+      if (s.getClass().equals(cls)) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  //for mainTower crosshair
+  public void initCrossHairs() {
+    guiNode.detachAllChildren();
+    guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+    BitmapText ch = new BitmapText(guiFont, false);
+    ch.setSize(guiFont.getCharSet().getRenderedSize() * 2);
+    ch.setText("+");
+    ch.setLocalTranslation( // center
+            settings.getWidth() / 2 - guiFont.getCharSet().getRenderedSize() / 3 * 2,
+            settings.getHeight() / 2 + ch.getLineHeight() / 2 + -25, 0);
+    guiNode.attachChild(ch);
+  }
 }
